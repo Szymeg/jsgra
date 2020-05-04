8ae315fe8ee392115ed03e59e21dbc41449b8e88
Szymeg <szymonandrzejewski9@gmail.com>
2020-05-04 20:47:55 +0200

    Js changes
    description

--- js.js
+++ js.js
@@ -1,16 +1,40 @@
-setTimeout(appear, 500);
-function appear(){
-document.getElementById('1').classList.remove('hide');
+//creating id list
+var id_list = [];
+for (var i = 1; i <= 9; i++) {
+    id_list.push(i);
 }
-setTimeout(close, 1000);
-function close(){
-document.getElementById('1').className += ' hide';
-}
+console.log(id_list);
+console.log(id_list[8])
+console.log(15);
+console.log('15 ale tekst');
+//looping the appear function
+id_list.forEach(element => {
+    console.log(element)
+    var n = element.toString();
+    console.log(n)
+    document.getElementById(n).classList.remove('hide');
+    
+    setTimeout(close, 1000);
+    function close(){
+    document.getElementById(n).className += ' hide';
+    }
+    
+});
 
+/*appear and disappear without interacting
+setTimeout(appear, 500);
+    function appear(){
+    document.getElementById('1').classList.remove('hide');
+    }
+setTimeout(close, 1000);
+    function close(){
+    document.getElementById('1').className += ' hide';
+    }*/
+
+//score counter
 var a = 0;
 console.log(a);
-
-function score () {
+function score (id) {
 document.getElementById("score").innerHTML = a+=1;
-document.getElementById("1").classList.add('hide');
+document.getElementById(id).classList.add('hide');
 }
